9e84c64484782cf055bf13d51cd6215b3d47f525
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2015-01-23 00:11:40 -0600

    cuerpo

--- scratcher/models.py
+++ scratcher/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-	name = models.CharField(max_lenght=100, help_text="Nombre del proyecto")
-	path = models.CharField(max_lenght=300, help_text="Ruta del proyecto")
+	name = models.CharField(max_length=100, help_text="Nombre del proyecto")
+	path = models.CharField(max_length=300, help_text="Ruta del proyecto")
 	
--- scratcher/views.py
+++ scratcher/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def pruebas(request):
+	return render(
+		request,
+		"test.html",
+		{}
+	)
